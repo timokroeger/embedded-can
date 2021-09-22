f6cdf56e37bb92e4e14935ae8b02543bc378b1a3
Timo Kröger <timokroeger93@gmail.com>
2021-09-22 19:16:10 +0200

    Move non-blocking trait to `nb` module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![no_std]
 
 pub mod blocking;
+pub mod nb;
 
 mod id;
 
@@ -44,32 +45,4 @@ pub trait Frame: Sized {
 
     /// Returns the frame data (0..8 bytes in length).
     fn data(&self) -> &[u8];
-}
-
-/// A CAN interface that is able to transmit and receive frames.
-pub trait Can {
-    /// Associated frame type.
-    type Frame: Frame;
-
-    /// Associated error type.
-    type Error;
-
-    /// Puts a frame in the transmit buffer to be sent on the bus.
-    ///
-    /// If the transmit buffer is full, this function will try to replace a pending
-    /// lower priority frame and return the frame that was replaced.
-    /// Returns `Err(WouldBlock)` if the transmit buffer is full and no frame can be
-    /// replaced.
-    ///
-    /// # Notes for implementers
-    ///
-    /// * Frames of equal identifier shall be transmited in FIFO fashion when more
-    ///   than one transmit buffer is available.
-    /// * When replacing pending frames make sure the frame is not in the process of
-    ///   being send to the bus.
-    fn try_transmit(&mut self, frame: &Self::Frame)
-        -> nb::Result<Option<Self::Frame>, Self::Error>;
-
-    /// Returns a received frame if available.
-    fn try_receive(&mut self) -> nb::Result<Self::Frame, Self::Error>;
 }
---
+++ src/nb.rs
@@ -0,0 +1,28 @@
+
+/// A CAN interface that is able to transmit and receive frames.
+pub trait Can {
+    /// Associated frame type.
+    type Frame: crate::Frame;
+
+    /// Associated error type.
+    type Error;
+
+    /// Puts a frame in the transmit buffer to be sent on the bus.
+    ///
+    /// If the transmit buffer is full, this function will try to replace a pending
+    /// lower priority frame and return the frame that was replaced.
+    /// Returns `Err(WouldBlock)` if the transmit buffer is full and no frame can be
+    /// replaced.
+    ///
+    /// # Notes for implementers
+    ///
+    /// * Frames of equal identifier shall be transmited in FIFO fashion when more
+    ///   than one transmit buffer is available.
+    /// * When replacing pending frames make sure the frame is not in the process of
+    ///   being send to the bus.
+    fn try_transmit(&mut self, frame: &Self::Frame)
+        -> nb::Result<Option<Self::Frame>, Self::Error>;
+
+    /// Returns a received frame if available.
+    fn try_receive(&mut self) -> nb::Result<Self::Frame, Self::Error>;
+}
